2d153878797c519e5f8f8216c00d33f84f5f6cda
Phil BStorm <philippe.haerens@bstorm.be>
2026-01-13 11:14:56 +0100

    demo 2 notion de class

---
+++ CoursPOO.demo02notionclass/Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursPOO.demo02notionclass.Models
+{
+    internal class Person
+    {
+        public string Email { get; set; }
+        public int Age { get; init; }
+        private string _Password;
+
+        public string Password
+        {
+            get
+            {
+                //return _Password;
+                return new string('*', _Password.Length); // "Test" deviendra "****"
+            }
+        }
+
+        public Person(string email, string password, int age)
+        {
+            Email = email;
+            _Password = password;
+            Age = age;
+        }
+
+        public bool ComparePassword(string pass)
+        {
+            return pass == _Password;
+        }
+
+        public void Vieillir()
+        {
+            // Age est marqué comme "init" donc ne peut pas être ré-affecter en dehors de l'initialisation
+            // Age += 1;
+        }
+    }
+}
---
+++ CoursPOO.demo02notionclass/Program.cs
@@ -0,0 +1,37 @@
+﻿using CoursPOO.demo02notionclass.Models;
+
+namespace CoursPOO.demo02notionclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Person phil = new Person("mauvaisemail@mail.com", "UnMotDePasse");
+            Person alex = new Person("alex@mail.com", "SuperMotDePasse");
+
+            // on utiliser le setter (du champs public)
+            phil.Email = "philippe.haerens@bstorm.be";
+            // on utiliser le getter (du champs public)
+            Console.WriteLine(phil.Email);
+
+            // ne fonctionne pas car _Password est en privé
+            //phil._Password = "Test123=";
+            //Console.WriteLine(phil._Password);
+            Console.WriteLine(phil.Password);
+
+            string mdp = Console.ReadLine();
+            // savoir si j'ai tapé le bon mot de passe de phil
+            bool goodPassword = phil.ComparePassword(mdp);
+            if (goodPassword)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("KO");
+            }
+        }
+    }
+}
