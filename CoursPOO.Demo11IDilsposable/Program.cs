c89c9fda2c6e0d026a6c0efb6880766f1bd01ac3
Phil BStorm <philippe.haerens@bstorm.be>
2026-01-19 13:01:03 +0100

    Demo IDiposable

---
+++ CoursPOO.Demo11IDilsposable/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CoursPOO.Demo11IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, IDisposable!");
+            using (TalkieWalkie tw1 = new TalkieWalkie("Sacha"))
+            {
+                tw1.Parler("Allo?");
+                using (TalkieWalkie tw2 = new TalkieWalkie("Brock"))
+                {
+                    tw2.Parler("Hello, c'est Brock");
+                    tw1.Parler("Hello, c'est Sacha");
+                }
+            }
+        }
+    }
+}
---
+++ CoursPOO.Demo11IDilsposable/TalkieWalkie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursPOO.Demo11IDisposable
+{
+    internal class TalkieWalkie: IDisposable
+    {
+        public string Nom { get; private set; }
+
+        public TalkieWalkie(string nom)
+        {
+            Nom = nom;
+            Console.WriteLine($"{nom} est connecté");
+        }
+
+        public void Parler(string message)
+        {
+            Console.WriteLine($"{Nom} {DateTime.Now.ToShortTimeString()} : {message}");
+            Console.WriteLine("A vous!");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{Nom} s'est déconnecté");
+        }
+    }
+}
