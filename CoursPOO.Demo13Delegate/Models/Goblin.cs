ead8c2efbc932f8c74265aecb69b82a9b6a39b70
Phil BStorm <philippe.haerens@bstorm.be>
2026-01-19 16:52:43 +0100

    Demo delegate

---
+++ CoursPOO.Demo13Delegate/Models/Goblin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursPOO.Demo13Delegate.Models
+{
+    //1) Créer le type de notre delegate "Mort"
+    delegate void Mort();
+
+    internal class Goblin
+    {
+        private int _Pv = 10;
+
+        // 2) On prépare la propriété qui stock les événements
+        public Mort MortDel;
+
+        public Goblin()
+        {
+            MortDel += TombeAuSol;
+        }
+
+        public void PrendreDegat(int montant)
+        {
+            _Pv -= montant;
+
+            if (_Pv <= 0)
+            {
+                if (MortDel is not null)
+                {
+                    // 3) Appel la delegate
+                    MortDel();
+                }
+            }
+        }
+
+        public void TombeAuSol()
+        {
+            Console.WriteLine("Aiiiiiiiie");
+        }
+    }
+}
---
+++ CoursPOO.Demo13Delegate/Program.cs
@@ -0,0 +1,51 @@
+﻿using CoursPOO.Demo13Delegate.Models;
+
+namespace CoursPOO.Demo13Delegate
+{
+    internal class Program
+    {
+        static bool continuerDeTaper = true;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Delegate!");
+            Goblin g1 = new Goblin();
+
+            // 4) Abonnement à la propriété
+            g1.MortDel += Paillette;
+            g1.MortDel += VictoryScreen;
+            g1.MortDel += VictoryScreen;
+            g1.MortDel += HeroDance;
+            g1.MortDel += VictoryScreen;
+            g1.MortDel += IlEstMort;
+
+            while (continuerDeTaper)
+            {
+                Console.WriteLine("Taper le goblin!!!");
+                g1.PrendreDegat(3);
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void Paillette()
+        {
+            Console.WriteLine("🎊🎊🎊");
+        }
+
+        public static void VictoryScreen()
+        {
+            Console.WriteLine("☆*: .｡. o(≧▽≦)o .｡.:*☆");
+        }
+
+        public static void HeroDance()
+        {
+            Console.WriteLine("🕺🕺🕺🕺🕺🕺");
+        }
+
+        public static void IlEstMort()
+        {
+            continuerDeTaper = false;
+        }
+    }
+}
